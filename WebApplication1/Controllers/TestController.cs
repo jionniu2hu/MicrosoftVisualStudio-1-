852eccd6b6497ef5b9f972ea6a40db5e28255cbc
fortissimo <2655843602@qq.com>
2018-12-06 17:23:48 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "你好，MVC";
+        }
+    }
+}
